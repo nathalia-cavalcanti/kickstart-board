48b9e3e089e3eb58b2d2971ef8cf6ab4bc26eda1
Nathalia Cavalcanti <nathcvcanti@gmail.com>
2024-06-11 00:12:34 -0300

    fix: fix api path delete lists and cards

--- src/store/actions/deleteList.ts
+++ src/store/actions/deleteList.ts
@@ -1,8 +1,9 @@
 import List from '@/typings/list';
 import { API_MOCK } from '@/service/mock';
+import Board from '@/typings/board';
 
-export const deleteList = async function (this: any, listId: List['id']) {
-  await API_MOCK.delete(`/api/lists/${listId}`);
+export const deleteList = async function (this: any, listId: List['id'], boardId: Board['id']) {
+  await API_MOCK.delete(`/api/boards/${boardId}/lists/${listId}`);
   this.lists = this.lists.filter((item: List) => item.id !== listId);
   this.sortLists();
 };
