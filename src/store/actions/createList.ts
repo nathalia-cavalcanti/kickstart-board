48c861d171334b18ea577bd41592657a24ebb759
Nathalia Cavalcanti <nathcvcanti@gmail.com>
2024-06-11 13:21:53 -0300

    fix: fix create list position order

--- src/store/actions/createList.ts
+++ src/store/actions/createList.ts
@@ -2,8 +2,10 @@ import Board from '@/typings/board';
 import { API_MOCK } from '@/service/mock';
 
 export const createList = async function (this: any, boardId: Board['id'], name: string) {
+  const order = this.lists[this.lists.length - 1].order + 1;
+
   await API_MOCK
-    .post('/api/lists', { boardId, name, order: this.lists.length })
+    .post('/api/lists', { boardId, name, order })
     .then(({ data }) => {
       data.cards = [];
       this.lists.push(data);
