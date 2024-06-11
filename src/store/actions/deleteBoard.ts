c5ac4a2483ba07c7e6188c6793821392b12527f3
Nathalia Cavalcanti <nathcvcanti@gmail.com>
2024-06-11 13:54:00 -0300

    fix: fix patch and delete board

--- src/store/actions/deleteBoard.ts
+++ src/store/actions/deleteBoard.ts
@@ -5,8 +5,9 @@ export const deleteBoard = async function (this: any, boardId: Board['id']) {
   await API_MOCK
     .delete(`/api/boards/${boardId}`)
     .then(() => {
+      this.boardList.all = this.boardList.all.filter((board: Board) => board.id !== boardId)
       this.showNotification('Board was deleted', false);
-    })
+    }) 
     .catch((e) => {
       console.log(e);
       this.showNotification('Board could not be deleted', true);
--- src/store/actions/patchBoard.ts
+++ src/store/actions/patchBoard.ts
@@ -2,8 +2,9 @@ import Board from '@/typings/board';
 import { API_MOCK } from '@/service/mock';
 
 export const patchBoard = async function (this: any, board: Board, payload: object) {
+  const boardId = board.id;
   const patchedBoard = await API_MOCK.put(`/api/boards/${board.id}`, payload);
-  const boardIndex = this.boardList.all.findIndex((board: Board) => board.id === board.id);
+  const boardIndex = this.boardList.all.findIndex((board: Board) => board.id === boardId);
   this.board = patchedBoard.data;
   this.boardList.all[boardIndex] = patchedBoard.data;
 };
